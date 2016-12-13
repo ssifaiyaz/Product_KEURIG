1212d45ddddb9377b6c348764a37bcf6bf619cb9
syedismailfaiyaz.s <syedismailfaiyaz.s@infosys.com>
2016-12-13 13:06:33 +0530

    ok

---
+++ KEURIG/src/loginPage/LoginOk.java
@@ -0,0 +1,10 @@
+package loginPage;
+
+public class LoginOk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ KEURIG/src/loginPage/loginCheck.java
@@ -0,0 +1,14 @@
+package loginPage;
+
+public class loginCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println(" code");
+		System.out.println(" added one more line of code");
+		System.out.println(" added two more line of code");
+
+	}
+
+}

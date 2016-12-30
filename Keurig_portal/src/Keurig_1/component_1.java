2a74fad41bd7aa296f9dff6aef161eb9584eb354
Megha.Goyal07 <Megha.Goyal07@TVMATP385814D.ad.infosys.com>
2016-12-30 18:26:00 +0530

    okk

--- Keurig_portal/src/Keurig_1/component_1.java
+++ Keurig_portal/src/Keurig_1/component_1.java
@@ -1,14 +1,31 @@
 package Keurig_1;
+import org.openqa.selenium.By;
 //import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 
 public class component_1 {
-
-	//public static void main(String[] args) {
+    public ChromeDriver driver;
+	public static main(String[] args){
 		// TODO Auto-generated method stub
 		 @Given("^open chrome and start$")
+		 public void open_chrome_and_start() throws Exception {
+		        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chittranjan01\\Desktop\\chromedriver_win32\\chromedriver.exe");
+		        ChromeDriver driver=new ChromeDriver();
+		        driver.manage().window().maximize();
+		        driver.get("https://timetracker.anuko.com/login.php");
 		 @When("^enter the invalid username and password$")
+		 public void enter_the_invalid_username_and_password() throws Exception {
+	           driver.findElement(By.id("Email")).sendKeys("harini.p@infy.com"); 
+	           driver.findElement(By.id("Password")).sendKeys("112");
+	          
 		 @Then("^user should not be able to login$")
-	}
-
-//}
+		 public void enter_the_invalid_username_and_password() throws Exception {
+			 driver.findElement(By.id("btn_login")).click();
+	           String error=driver.findElement(By.className("error")).getText();
+	                System.out.println("Error Message:"+error);
+	                Assert.assertEquals(error, "Incorrect login or password.");
+	                driver.close();  
+		 }}
---
+++ Keurig_portal/src/cucumber_Test/lonIN_test.java
@@ -0,0 +1,10 @@
+package cucumber_Test;
+
+public class lonIN_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

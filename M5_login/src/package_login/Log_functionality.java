41230cc84ca81bf6695cd1303890e30e4ecacf00
Neeraj Sharma <neeraj.sharma27@infosys.com>
2016-12-30 17:07:48 +0530

    ok

--- M5_login/src/package_login/Log_functionality.java
+++ M5_login/src/package_login/Log_functionality.java
@@ -2,36 +2,53 @@ package package_login;
 
 
 import java.io.File;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.write.Label;
-import jxl.write.WritableCell;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Log_functionality {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		WebDriver driver;
 		String url;
 		try {
 			
 			// counting no. of rows of Input data sheet  
-			Workbook login = Workbook.getWorkbook(new File("C:\\Users\\neeraj.sharma27\\Desktop\\KeurigLogin.xls"));
+			Workbook login = Workbook.getWorkbook(new File(System.getProperty("user.dir") + "\\Testdata\\KeurigLogin.xls"));
 			Sheet loginsheet = login.getSheet(0);
 			int rowcount = loginsheet.getRows();
 			System.out.println("rowcount is = "+ rowcount);
 			
-			for (int i = 1; i < rowcount; i++) {
+			File src =   new File("D:\\Book.xls");
+			
+			WritableWorkbook wb=Workbook.createWorkbook(src);
+			wb.createSheet("Final",0);
+			WritableSheet wsheet=wb.getSheet(0);
+			
+			Label data1=new Label(0,0,"Result"); 
+			wsheet.addCell(data1);
+			
+			ArrayList<String> arrayList = new ArrayList<String>();
+			
+			for (int i = 1; i <rowcount; i++) {
 				
 				// launching chrome driver
-				System.setProperty("webdriver.chrome.driver","C:\\Users\\neeraj.sharma27\\Desktop\\chromedriver.exe");
+				
+				//System.setProperty("webdriver.chrome.driver","C:\\Users\\neeraj.sharma27\\Desktop\\chromedriver.exe");
 				driver = new ChromeDriver();
-				url = "http://tvmatp367321d:8080/KeurigPortalDev/login.jsp";
+				url = "http://tvmatp324126d:8081/KeurigPortalDev/login.jsp";
 				driver.get(url);
 				
 				// taking data from Excel sheet and try logging in
@@ -47,26 +64,48 @@ public class Log_functionality {
 				driver.switchTo().window(tab2);
 				
 				
-				System.out.println(loginsheet.getCell(0, i).getContents()+"  has logged in");
-				driver.close();
+				
+				
+			 
+		Thread.sleep(3000);
+			WebElement ele=driver.findElement(By.xpath("html/body/h1"));
 
-			}
-			
-			// writing the status of test case in a new Excel Sheet
-			File excelfile=new File("D:\\result.xls");
-			WritableWorkbook writableworkbook=Workbook.createWorkbook(excelfile);
-			WritableSheet writablesheet=writableworkbook.createSheet("Sheet1", 0);
-			
-			 Label l=new Label(0, 0 , "Result");
-			 writablesheet.addCell(l);
-			for(int j=1;j<=3;j++){
-				WritableCell c=(WritableCell)new Label(0,j,"Pass");
-				writablesheet.addCell(c);
-			}
+			 String  test_title=ele.getText();
+			 
+				
+				
+				
+				if (test_title.equalsIgnoreCase("Welcome to Home Page")) {
 					
+					arrayList.add("pass");
+					
+				
+				}else{	
+					arrayList.add("fail");
+				}
+				
+				driver.close();
+			
+			}
+			
+		System.out.println(arrayList.size());
+		
+		for(int i=0;i<arrayList.size();i++){
+			Label data=new Label(0,i+1,arrayList.get(i));
+			wsheet.addCell(data);
+		}
+		wb.write();
+		wb.close();
+		
+		
+		
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
+			System.out.println("login unsuccessful");
+			
+			
 		}
+		
 
 	}
 
